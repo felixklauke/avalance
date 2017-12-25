d12d185d764bfcf7537f6580196f73d0fc53bf84
Felix Klauke <info@felix-klauke.de>
2017-12-25 17:30:01 +0100

    Fix the core refactoring.

--- core/src/main/java/de/felix_klauke/avalance/core/AvalanceFactory.java
+++ core/src/main/java/de/felix_klauke/avalance/core/AvalanceFactory.java
@@ -1,13 +1,13 @@
 package de.felix_klauke.avalance.core;
 
-import de.felix_klauke.avalance.core.config.AvalanceConfig;
+import de.felix_klauke.avalance.core.config.AvalanceCoreConfig;
 
 /**
  * @author Felix Klauke <fklauke@itemis.de>
  */
 public class AvalanceFactory {
 
-    public static Avalance createAvalance(AvalanceConfig avalanceConfig) {
-        return new AvalanceImpl(avalanceConfig);
+    public static Avalance createAvalance(AvalanceCoreConfig avalanceCoreConfig) {
+        return new AvalanceImpl(avalanceCoreConfig);
     }
 }
