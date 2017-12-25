e7ab3e04622122dc6de22482cff9f805466b1b1e
Felix Klauke <info@felix-klauke.de>
2017-12-25 17:26:11 +0100

    Add base url config and tests. (#14)

--- server/src/main/java/de/felix_klauke/avalance/server/config/AvalanceServerConfig.java
+++ server/src/main/java/de/felix_klauke/avalance/server/config/AvalanceServerConfig.java
@@ -1,7 +1,32 @@
 package de.felix_klauke.avalance.server.config;
 
 /**
+ * Basic config of the avalance server module.
+ *
  * @author Felix Klauke <fklauke@itemis.de>
  */
 public class AvalanceServerConfig {
+
+    /**
+     * The basic resource route everything is built on.
+     */
+    private final String baseUrl;
+
+    /**
+     * Create a new Config.
+     *
+     * @param baseUrl The base url.
+     */
+    public AvalanceServerConfig(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    /**
+     * Get the base url.
+     *
+     * @return The base url.
+     */
+    public String getBaseUrl() {
+        return baseUrl;
+    }
 }
