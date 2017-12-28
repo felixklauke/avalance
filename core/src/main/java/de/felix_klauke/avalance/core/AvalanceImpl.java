db197587e27766716ec35be4f501e7dc43cbcb9b
Felix Klauke <info@felix-klauke.de>
2017-12-28 10:42:28 +0100

    Update the location of the router. (#22)

--- core/src/main/java/de/felix_klauke/avalance/core/AvalanceImpl.java
+++ core/src/main/java/de/felix_klauke/avalance/core/AvalanceImpl.java
@@ -1,15 +1,33 @@
 package de.felix_klauke.avalance.core;
 
 import de.felix_klauke.avalance.core.config.AvalanceCoreConfig;
+import de.felix_klauke.avalance.core.routing.Router;
+import de.felix_klauke.avalance.core.routing.RouterImpl;
 
 /**
+ * The default implementation of {@link Avalance}.
+ *
  * @author Felix Klauke <fklauke@itemis.de>
  */
 public class AvalanceImpl implements Avalance {
 
+    /**
+     * The underlying core config.
+     */
     private final AvalanceCoreConfig coreConfig;
 
+    /**
+     * The internal router.
+     */
+    private final Router router;
+
+    /**
+     * Create a new avalance instance.
+     *
+     * @param coreConfig The core config.
+     */
     AvalanceImpl(AvalanceCoreConfig coreConfig) {
         this.coreConfig = coreConfig;
+        this.router = new RouterImpl();
     }
 }
