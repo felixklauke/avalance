18a73b4d106f4e08912822d78b3943a863a4d7e5
Felix Klauke <info@felix-klauke.de>
2018-01-12 09:41:59 +0100

    Feature/request lifecycle (#28)
    * Add test for utils.
    
    * Add full request lifecycle including tests for utils and netty infrastructure.
    
    * Fix handler test.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Fix channel initializer test.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Increment handler count.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Make test dumb for now.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Add test for server impl.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Add some router tests.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Add dummy method to router request test.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Add full test for netty utils.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Cover some http handler aspects.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- server/src/test/java/de/felix_klauke/avalance/server/http/AvalanceHttpServerHandlerTest.java
+++ server/src/test/java/de/felix_klauke/avalance/server/http/AvalanceHttpServerHandlerTest.java
@@ -1,310 +1,51 @@
 package de.felix_klauke.avalance.server.http;
 
-import de.felix_klauke.avalance.server.AvalanceServerFactory;
-import de.felix_klauke.avalance.server.config.AvalanceServerConfig;
-import io.netty.buffer.ByteBufAllocator;
-import io.netty.channel.*;
-import io.netty.handler.codec.DecoderResult;
-import io.netty.handler.codec.http.HttpHeaders;
-import io.netty.handler.codec.http.HttpMethod;
+import de.felix_klauke.avalance.core.routing.RouterContext;
+import de.felix_klauke.avalance.core.routing.RouterRequest;
+import de.felix_klauke.avalance.server.AvalanceServer;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.HttpRequest;
-import io.netty.handler.codec.http.HttpVersion;
-import io.netty.util.Attribute;
-import io.netty.util.AttributeKey;
-import io.netty.util.concurrent.EventExecutor;
+import io.netty.handler.codec.http.HttpResponse;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.net.SocketAddress;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 /**
  * @author Felix Klauke <fklauke@itemis.de>
  */
+@RunWith(MockitoJUnitRunner.class)
 public class AvalanceHttpServerHandlerTest {
+
+    @Mock
+    AvalanceServer avalanceServer;
+    @Mock
+    ChannelHandlerContext channelHandlerContext;
+    @Mock
+    HttpRequest httpRequest;
 
     private AvalanceHttpServerHandler avalanceHttpServerHandler;
 
     @Before
     public void setUp() {
-        avalanceHttpServerHandler = new AvalanceHttpServerHandler(AvalanceServerFactory.createAvalanceServer(new AvalanceServerConfig("")));
+        avalanceHttpServerHandler = new AvalanceHttpServerHandler(avalanceServer);
     }
 
     @Test
     public void channelRead0() {
+        ChannelFuture channelFuture = Mockito.mock(ChannelFuture.class);
+
+        Mockito.when(channelHandlerContext.writeAndFlush(Mockito.any(HttpResponse.class))).thenReturn(channelFuture);
+
         try {
-            avalanceHttpServerHandler.channelRead0(new ChannelHandlerContext() {
-                @Override
-                public Channel channel() {
-                    return null;
-                }
-
-                @Override
-                public EventExecutor executor() {
-                    return null;
-                }
-
-                @Override
-                public String name() {
-                    return null;
-                }
-
-                @Override
-                public ChannelHandler handler() {
-                    return null;
-                }
-
-                @Override
-                public boolean isRemoved() {
-                    return false;
-                }
-
-                @Override
-                public ChannelHandlerContext fireChannelRegistered() {
-                    return null;
-                }
-
-                @Override
-                public ChannelHandlerContext fireChannelUnregistered() {
-                    return null;
-                }
-
-                @Override
-                public ChannelHandlerContext fireChannelActive() {
-                    return null;
-                }
-
-                @Override
-                public ChannelHandlerContext fireChannelInactive() {
-                    return null;
-                }
-
-                @Override
-                public ChannelHandlerContext fireExceptionCaught(Throwable cause) {
-                    return null;
-                }
-
-                @Override
-                public ChannelHandlerContext fireUserEventTriggered(Object evt) {
-                    return null;
-                }
-
-                @Override
-                public ChannelHandlerContext fireChannelRead(Object msg) {
-                    return null;
-                }
-
-                @Override
-                public ChannelHandlerContext fireChannelReadComplete() {
-                    return null;
-                }
-
-                @Override
-                public ChannelHandlerContext fireChannelWritabilityChanged() {
-                    return null;
-                }
-
-                @Override
-                public ChannelHandlerContext read() {
-                    return null;
-                }
-
-                @Override
-                public ChannelHandlerContext flush() {
-                    return null;
-                }
-
-                @Override
-                public ChannelPipeline pipeline() {
-                    return null;
-                }
-
-                @Override
-                public ByteBufAllocator alloc() {
-                    return null;
-                }
-
-                @Override
-                public <T> Attribute<T> attr(AttributeKey<T> key) {
-                    return null;
-                }
-
-                @Override
-                public <T> boolean hasAttr(AttributeKey<T> key) {
-                    return false;
-                }
-
-                @Override
-                public ChannelFuture bind(SocketAddress localAddress) {
-                    return null;
-                }
-
-                @Override
-                public ChannelFuture connect(SocketAddress remoteAddress) {
-                    return null;
-                }
-
-                @Override
-                public ChannelFuture connect(SocketAddress remoteAddress, SocketAddress localAddress) {
-                    return null;
-                }
-
-                @Override
-                public ChannelFuture disconnect() {
-                    return null;
-                }
-
-                @Override
-                public ChannelFuture close() {
-                    return null;
-                }
-
-                @Override
-                public ChannelFuture deregister() {
-                    return null;
-                }
-
-                @Override
-                public ChannelFuture bind(SocketAddress localAddress, ChannelPromise promise) {
-                    return null;
-                }
-
-                @Override
-                public ChannelFuture connect(SocketAddress remoteAddress, ChannelPromise promise) {
-                    return null;
-                }
-
-                @Override
-                public ChannelFuture connect(SocketAddress remoteAddress, SocketAddress localAddress, ChannelPromise promise) {
-                    return null;
-                }
-
-                @Override
-                public ChannelFuture disconnect(ChannelPromise promise) {
-                    return null;
-                }
-
-                @Override
-                public ChannelFuture close(ChannelPromise promise) {
-                    return null;
-                }
-
-                @Override
-                public ChannelFuture deregister(ChannelPromise promise) {
-                    return null;
-                }
-
-                @Override
-                public ChannelFuture write(Object msg) {
-                    return null;
-                }
-
-                @Override
-                public ChannelFuture write(Object msg, ChannelPromise promise) {
-                    return null;
-                }
-
-                @Override
-                public ChannelFuture writeAndFlush(Object msg, ChannelPromise promise) {
-                    return null;
-                }
-
-                @Override
-                public ChannelFuture writeAndFlush(Object msg) {
-                    return null;
-                }
-
-                @Override
-                public ChannelPromise newPromise() {
-                    return null;
-                }
-
-                @Override
-                public ChannelProgressivePromise newProgressivePromise() {
-                    return null;
-                }
-
-                @Override
-                public ChannelFuture newSucceededFuture() {
-                    return null;
-                }
-
-                @Override
-                public ChannelFuture newFailedFuture(Throwable cause) {
-                    return null;
-                }
-
-                @Override
-                public ChannelPromise voidPromise() {
-                    return null;
-                }
-            }, new HttpRequest() {
-                @Override
-                public HttpMethod getMethod() {
-                    return null;
-                }
-
-                @Override
-                public HttpMethod method() {
-                    return null;
-                }
-
-                @Override
-                public HttpRequest setMethod(HttpMethod method) {
-                    return null;
-                }
-
-                @Override
-                public String getUri() {
-                    return null;
-                }
-
-                @Override
-                public String uri() {
-                    return null;
-                }
-
-                @Override
-                public HttpRequest setUri(String uri) {
-                    return null;
-                }
-
-                @Override
-                public HttpRequest setProtocolVersion(HttpVersion version) {
-                    return null;
-                }
-
-                @Override
-                public HttpVersion getProtocolVersion() {
-                    return null;
-                }
-
-                @Override
-                public HttpVersion protocolVersion() {
-                    return null;
-                }
-
-                @Override
-                public HttpHeaders headers() {
-                    return null;
-                }
-
-                @Override
-                public DecoderResult getDecoderResult() {
-                    return null;
-                }
-
-                @Override
-                public void setDecoderResult(DecoderResult result) {
-
-                }
-
-                @Override
-                public DecoderResult decoderResult() {
-                    return null;
-                }
-            });
+            avalanceHttpServerHandler.channelRead0(channelHandlerContext, httpRequest);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        Mockito.verify(avalanceServer).processRequest(Mockito.any(RouterContext.class), Mockito.any(RouterRequest.class));
     }
 }
