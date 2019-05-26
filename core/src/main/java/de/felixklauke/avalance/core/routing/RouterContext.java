42964e437e9a6963fb7fe11e8bde883e563a7728
Felix Klauke <info@felix-klauke.de>
2019-05-26 07:58:55 +0200

    Apply lombok in core.

--- core/src/main/java/de/felixklauke/avalance/core/routing/RouterContext.java
+++ core/src/main/java/de/felixklauke/avalance/core/routing/RouterContext.java
@@ -1,32 +1,23 @@
 package de.felixklauke.avalance.core.routing;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * The basic representation of the routing flow.
  *
  * @author Felix Klauke <info@felix-klauke.de>
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class RouterContext {
 
     /**
      * The result of the routing flow.
      */
     private RoutingResult routingResult = new SkeletonRoutingResult();
-
-    /**
-     * Get the result of the routing result.
-     *
-     * @return The result.
-     */
-    public RoutingResult getRoutingResult() {
-        return routingResult;
-    }
-
-    /**
-     * Set the result of the routing context.
-     *
-     * @param routingResult The result.
-     */
-    public void setRoutingResult(RoutingResult routingResult) {
-        this.routingResult = routingResult;
-    }
 }
--- core/src/main/java/de/felixklauke/avalance/core/routing/RoutingResult.java
+++ core/src/main/java/de/felixklauke/avalance/core/routing/RoutingResult.java
@@ -1,11 +1,14 @@
 package de.felixklauke.avalance.core.routing;
 
+import lombok.Data;
+
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author Felix Klauke <info@felix-klauke.de>
  */
+@Data
 public class RoutingResult {
 
     /**
@@ -22,42 +25,4 @@ public class RoutingResult {
      * The meta data of the resulting service.
      */
     private final Map<String, Object> metadata = new HashMap<>();
-
-    /**
-     * Create a new routing result.
-     *
-     * @param hostName The host name.
-     * @param port     The port.
-     */
-    public RoutingResult(String hostName, int port) {
-        this.hostName = hostName;
-        this.port = port;
-    }
-
-    /**
-     * Get the host name of the resulting service.
-     *
-     * @return The host name.
-     */
-    public String getHostName() {
-        return hostName;
-    }
-
-    /**
-     * Get the port of the resulting service.
-     *
-     * @return The port.
-     */
-    public int getPort() {
-        return port;
-    }
-
-    /**
-     * Get the meta data of the resulting service.
-     *
-     * @return The meta data.
-     */
-    public Map<String, Object> getMetadata() {
-        return metadata;
-    }
 }
